1a3a20d74de9890bc79ae945b67f5ef39c5d1dc9
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-04-26 21:32:46 +0000

    .

--- 02-trabalhandoComVariaveis.js
+++ 02-trabalhandoComVariaveis.js
@@ -3,4 +3,3 @@ console.log("Trabalhando com variáveis");
 //boa pratica é sempre declarar o const
 const idade = 28;
 const nome = "victor"
-let ano = 2024;
---
+++ 05-atribuicaoDeVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Atribuicao de Variaveis");
+
+const idade = 29;
+let primeiroNome = "victor";
+const sobrenome = "teixeira";
+
+console.log(primeiroNome, sobrenome);
+console.log(`meu nome é ${primeiroNome} ${sobrenome}`);
+
+primeiroNome = primeiroNome + sobrenome;
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(primeiroNome);
+console.log(nomeCompleto);
+
