4ec6ac0f565b41ae39c97c884df8c3b9833eae1e
victoremannuel <vic.emannuel@gmail.com>
2024-06-12 20:51:56 -0300

    .

--- curso2OrientadoObjetos/index.js
+++ curso2OrientadoObjetos/index.js
@@ -1,21 +1,46 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
+};
+
+class ContaCorrente {
+    //convenção o _ representar um atributo privado
+    // _saldo;
+    //# também representa que é privado como no Java
+    #saldo;
+    #agencia;
+
+    sacar(valor) {
+        if (this.#saldo >= valor) {
+            this.#saldo -= valor;
+            return valor;
+        } else {
+            console.log("saldo insuficiente");
+        }
+    }
+
+    depositar(valor) {
+        if (valor <= 0) {
+            console.log("erro: valor negativo");
+        }
+        this.#saldo += valor;
+    }
 };
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "victor";
 cliente1.cpf = 11122233300;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "ana";
 cliente2.cpf = 22233344400;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const ccVictor = new ContaCorrente();
+// ccVictor.#saldo = 0;
+// ccVictor.#agencia = 1001;
+ccVictor.depositar(100);
+ccVictor.sacar(10);
+
+console.log(cliente1);
+// console.log("saldo:", ccVictor.#saldo);
+console.log(cliente2);
