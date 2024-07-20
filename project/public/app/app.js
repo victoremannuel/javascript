f68df26fa38742949eadedc282c0dddcb5e4ee03
victoremannuel <vic.emannuel@gmail.com>
2024-07-20 19:47:13 -0300

    .

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -5,15 +5,16 @@
 import { handleStatus, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 
+const sumItems = notas => notas
+    .$flatMap(nota => nota.itens)
+    .filter(item => item.codigo == '2143')
+    .reduce((total, item) => total + item.valor, 0);
+
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
             .then(handleStatus) //importei do utils
-            .then(notas => notas
-                .$flatMap(nota => nota.itens)
-                .filter(item => item.codigo == '2143')
-                .reduce((total, item) => total + item.valor, 0)
-            )
+            .then(sumItems)
             .then(console.log)
-            .catch(error => console.log(error));
+            .catch(console.log);
