04fcea216b3d869cecf8f005bbd927e94d9d34a6
victoremannuel <vic.emannuel@gmail.com>
2024-07-20 18:24:50 -0300

    .

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,3 +1,13 @@
+// document
+//     .querySelector('#myButton')
+//     .onclick = () => alert('oi');
+
+import { handleStatus } from './utils/promise-helpers.js';
+
 document
-.querySelector('#myButton')
-.onclick = () => alert('oi');
+    .querySelector('#myButton')
+    .onclick = () =>
+        fetch('http://localhost:3000/notas')
+            .then(handleStatus) //importei do utils
+            .then(notas => console.log(notas))
+            .catch(error => console.log(error));
