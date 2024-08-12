49c30cabab6225f3f7d2a7d179440e864e90c1a2
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-12 22:53:50 +0000

    .

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -7,16 +7,10 @@ import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 
-const sumItems = code => notas => 
-    notas.$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0)
-
 document
 .querySelector('#myButton')
 .onclick = () => 
     service
-    .listAll()
-    .then(sumItems('2143'))
+    .sumItems('2143')
     .then(console.log)
     .catch(console.log);
