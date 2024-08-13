cb8d6847ffa88bb167146b674f1228f8100e01dd
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-13 19:28:18 -0300

    Delete project directory

--- project/public/app/utils/array-helpers.js
+++
@@ -1,6 +0,0 @@
-if (!Array.prototype.$flatMap) {
-    Array.prototype.$flatMap = function (callback) {
-        this.map(callback).reduce((destArray, array) =>
-            destArray.concat(array), [])
-    }
-}
