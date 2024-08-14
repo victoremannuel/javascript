e6013d6ea20bf53253f486895f80e9eeed105f3b
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-14 21:17:20 +0000

    .

---
+++ typescript/app/controllers/negociacao-controller.ts
@@ -0,0 +1,22 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+
+    private inputData : HTMLInputElement;
+    private inputQuantidade : HTMLInputElement;
+    private inputValor : HTMLInputElement;
+
+    constructor (){
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona () {
+        const exp = /-/g; //substitui tudo que está sendo procurado pelo conteudo entre barras
+        const date = new Date(this.inputData.value.replace(exp, ',')); // procura todas as virculas e substitui usando o exp
+        const quantidade = parseInt(this.inputQuantidade.value); //converte para inteiro
+        const valor = parseFloat(this.inputValor.value); //converte para float
+        const negociacao = new Negociacao(date, quantidade, valor);
+    }
+};
