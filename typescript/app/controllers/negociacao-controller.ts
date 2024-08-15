f83a80c993825e5da2f835f3a9069b3add63552a
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-15 20:24:58 +0000

    .

--- typescript/app/controllers/negociacao-controller.ts
+++ typescript/app/controllers/negociacao-controller.ts
@@ -1,5 +1,6 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
 
@@ -7,11 +8,13 @@ export class NegociacaoController {
     private inputQuantidade : HTMLInputElement;
     private inputValor : HTMLInputElement;
     private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor () {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update();
     };
 
     adiciona () : void {
