8eb2801abbb2adf3af5641624399e1145fa2e707
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-15 14:32:40 +0000

    .

--- typescript/app/controllers/negociacao-controller.ts
+++ typescript/app/controllers/negociacao-controller.ts
@@ -2,7 +2,7 @@ import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacaoController {
 
-    private inputData : HTMLInputElement;
+    private inputData : HTMLInputElement; //tipando o atributo
     private inputQuantidade : HTMLInputElement;
     private inputValor : HTMLInputElement;
 
@@ -10,13 +10,26 @@ export class NegociacaoController {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
-    }
+    };
 
-    adiciona () {
+    adiciona () : void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    };
+    
+    criaNegociacao () : Negociacao /*tipando o método*/ {
         const exp = /-/g; //substitui tudo que está sendo procurado pelo conteudo entre barras
         const date = new Date(this.inputData.value.replace(exp, ',')); // procura todas as virculas e substitui usando o exp
         const quantidade = parseInt(this.inputQuantidade.value); //converte para inteiro
         const valor = parseFloat(this.inputValor.value); //converte para float
-        const negociacao = new Negociacao(date, quantidade, valor);
-    }
+        return new Negociacao(date, quantidade, valor);
+    };
+
+    limparFormulario () : void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    };
 };
--- typescript/dist/js/controllers/negociacao-controller.js
+++ typescript/dist/js/controllers/negociacao-controller.js
@@ -5,12 +5,27 @@ export class NegociacaoController {
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
+    ;
     adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    ;
+    criaNegociacao() {
         const exp = /-/g; //substitui tudo que está sendo procurado pelo conteudo entre barras
         const date = new Date(this.inputData.value.replace(exp, ',')); // procura todas as virculas e substitui usando o exp
         const quantidade = parseInt(this.inputQuantidade.value); //converte para inteiro
         const valor = parseFloat(this.inputValor.value); //converte para float
-        const negociacao = new Negociacao(date, quantidade, valor);
+        return new Negociacao(date, quantidade, valor);
     }
+    ;
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+    ;
 }
 ;
