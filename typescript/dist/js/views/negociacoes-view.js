002a3d7bd406158017efb09b4eb9e2031e34b843
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-15 20:44:28 +0000

    .

--- typescript/dist/js/views/negociacoes-view.js
+++ typescript/dist/js/views/negociacoes-view.js
@@ -3,7 +3,7 @@ export class NegociacoesView {
         this.elemento = document.querySelector(seletor);
     }
     ;
-    template() {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -14,14 +14,27 @@ export class NegociacoesView {
                     </tr>
                 </thead>
                 <tbody>
-
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr>
+                                <td>
+                                ${new Intl.DateTimeFormat()
+                .format(negociacao.data)}
+                                </td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                            </tr>
+                        `;
+        }).join('')}
                 </tbody>
             </table>
         `;
     }
     ;
-    update() {
-        this.elemento.innerHTML = this.template();
+    update(model) {
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
     }
     ;
 }
