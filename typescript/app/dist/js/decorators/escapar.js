0f165774668bacd84211db4700f39874e9e7b007
Víctor Emannuel S Teixeira <105996637+victoremannuel@users.noreply.github.com>
2024-08-20 23:48:21 +0000

    .

---
+++ typescript/app/dist/js/decorators/escapar.js
@@ -0,0 +1,13 @@
+export function escapar(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        let retorno = metodoOriginal.apply(this, args);
+        if (typeof retorno === 'string') {
+            retorno = retorno.replace(/<script>[\s\S]*?<\/script>/g, '');
+        }
+        ;
+        return retorno;
+    };
+    return descriptor;
+}
+;
--- typescript/app/src/views/view.ts
+++ typescript/app/src/views/view.ts
@@ -1,29 +1,22 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-execucao.js";
 
 export abstract class View<T> { //tipo genérico que é definido pela classe filha quando extende a mãe
 //classe abstrata impede sua instancia direta, ou seja, fora do construtor
 
     protected elemento : HTMLElement; //para que as outras classes(filhas) que estão importando View possam acessar o elemento
-    private escapar : boolean = false;
 
-    constructor (seletor : string, escapar ?: boolean) {
+    constructor (seletor : string) {
         const elemento = document.querySelector(seletor)
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         }else{
             throw Error ('seletor não existe no DOM, verifique.')
-        }
-        if (escapar) {
-            this.escapar = escapar;
         };
     };
 
-    @logarTempoDeExecucao()
     public update (model : T) : void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/,'');
-        };
         this.elemento.innerHTML = template;
     };
 
